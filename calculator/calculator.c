8a8c0b61fa38638ac6162231c9777e8e958e6421
Md Altamash Ansari <132382652+mdaltamashansari@users.noreply.github.com>
2025-10-29 21:56:06 +0530

    commit

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -7,35 +7,28 @@ int main()
     float result;
     printf("Enter the first number: ");
     scanf("%f",&num1);
-
     printf("Enter the opertion: ");
     scanf(" %c",&op);
-
     printf("Enter the Second number: ");
     scanf("%f",&num2);
-
     switch (op)
     {
     case '-':
     result = num1-num2;
     printf("%f\n",result);
-        break;
-    
+      break;
     case '+':
     result = num1+num2;
     printf("%.2f\n",result);
         break;
-
     case '*':
     result = num1*num2;
     printf("%f\n",result);
         break;
-
     case '/':
     result = num1/num2;
     printf("%f\n",result);
         break;
-    
     default:
     printf("the operator is not valid");
         break;
